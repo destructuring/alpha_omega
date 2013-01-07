300cd617a366eea3b18419ce2686554d1bcfd91f
Tom Bombadil <amanibhavam@destructuring.org>
2013-01-06 20:58:19 -0800

    remove net::ssh overrides, getting-started, pod specific hacks app dir mode is 0750, removes world permissions

--- lib/alpha_omega/config/deploy.rb
+++ lib/alpha_omega/config/deploy.rb
@@ -1,4 +1,3 @@
-require 'alpha_omega/config/deploy_ssh'
 require 'alpha_omega/config/deploy_challenge'
 require 'alpha_omega/config/deploy_notify'
 require 'alpha_omega/config/deploy_localhost'
--- lib/alpha_omega/config/deploy_localhost.rb
+++ lib/alpha_omega/config/deploy_localhost.rb
@@ -9,7 +9,7 @@ Capistrano::Configuration.instance(:must_exist).load do |config|
         set :group, %x(id -g -n).chomp
         set :root_user, %x(id -u -n).chomp
         set :root_group, %x(id -g -n).chomp
-        set :dir_perms, "0755"
+        set :dir_perms, "0750"
         set :bundler_options, "--path vendor/bundle"
         set :skip_scm, true
       end
