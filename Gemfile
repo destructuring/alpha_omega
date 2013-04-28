b259b3caad7a3d13a21f0fdfa474d04963b0a9c8
Tom Bombadil <amanibhavam@destructuring.org>
2013-04-27 23:16:27 -0700

    remove notifications and gems, update to latest capistrano

--- Gemfile
+++ Gemfile
@@ -1,18 +1,11 @@
 load File.join(File.dirname(__FILE__), 'lib', 'development.rb')
 
-source :rubygems
+source "https://rubygems.org"
 
 gemspec
 
 gem "net-ssh", "~> 2.4.0"
 gem "json"
-
-gem "capistrano-campfire"
-gem 'airbrake', "~> 3.1.1"
-gem "newrelic_rpm", "~> 3.3.4"
-gem "flowdock"
-
-gem "microwave"
 gem "opsdb"
 
 # vim: set ft=ruby:
--- lib/alpha_omega/config/deploy_notify.rb
+++ lib/alpha_omega/config/deploy_notify.rb
@@ -6,72 +6,11 @@ Capistrano::Configuration.instance(:must_exist).load do |config|
       task :default do
         if $deploy["notify"]
           unless skip_notifications
-            airbrake if $deploy["notify"].member? "airbrake"
-            newrelic if $deploy["notify"].member? "newrelic"
-            campfire if $deploy["notify"].member? "campfire"
-            flowdock if $deploy["notify"].member? "flowdock"
+            # run notifications here
           end
 
           email if $deploy["notify"].member? "email"
         end
-      end
-
-      task :campfire do
-        require 'capistrano/campfire'
-
-        set :campfire_options, 
-              :ssl => true,
-              :account => $deploy["notify"]["campfire"]["account"],
-              :room => $deploy["notify"]["campfire"]["room"],
-              :email => $deploy["notify"]["campfire"]["email"],
-              :token => $deploy["notify"]["campfire"]["token"]
-
-        begin 
-          campfire_room.speak notify_message
-        rescue
-          $stderr.puts "Campfire announcement failed"
-        end
-      end
-
-      task :airbrake do
-        require 'airbrake'
-        require 'airbrake_tasks'
-
-        Airbrake.configure do |abconfig|
-          abconfig.api_key = $deploy["notify"]["airbrake"]["api_key"]
-        end
-        
-        begin
-          AirbrakeTasks.deploy({
-            :rails_env      => dna['app_env'],
-            :scm_revision   => "#{real_revision} #{revision}",
-            :scm_repository => repository,
-            :local_username => ENV['_AO_DEPLOYER']
-          })
-        rescue EOFError
-          $stderr.puts "An error occurred during the Airtoad deploy notification."
-        end
-      end
-
-      task :flowdock do
-        require 'flowdock'
-
-        flow = Flowdock::Flow.new(:api_token => $deploy["notify"]["flowdock"]["api_token"])
-                    
-        flow.push_to_team_inbox(
-          :subject => "Application #{$deploy["application"]} deployed #deploy",
-          :content => "Application deployed successfully!", 
-          :tags => $deploy["notify"]["flowdock"]["tags"],
-          :source => "alpha_omega deployment",
-          :from => {
-            :address => $deploy["notify"]["flowdock"]["from"]["address"],
-            :name => $deploy["notify"]["flowdock"]["from"]["name"]
-          },
-          :project => $deploy["notify"]["flowdock"]["project"])
-      end 
-
-      task :newrelic do
-        require 'new_relic/recipes'
       end
 
       task :email do
